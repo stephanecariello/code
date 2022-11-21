81725f01cbf9761746191fcff01ace1f6eadb36d
stephanecariello <107881867+stephanecariello@users.noreply.github.com>
2022-11-21 21:22:38 +0100

    Add files via upload

--- mypage_rename_randomly.py
+++ mypage_rename_randomly.py
@@ -3,8 +3,7 @@ from zipfile import ZipFile
 
 
 # here the title   
-st.title
-("Welcome")
+st.title("Welcome")
 
 # list of uploaded files
 files=st.file_uploader("Sélectionnez les fichiers", accept_multiple_files=True)
@@ -14,23 +13,20 @@ list_nicknames=st.text_input("Surnoms:").split(" ")
 
 
 # zip file
-myZip=ZipFile("list_nicknames.zip","w")
+myZip=ZipFile("list_files.zip","w")
 
 for file in files:
-    before=file_name.split(".")[0]
-    after=file_name.split(".")[1]
+    before=file.split(".")[0]
+    after=file.split(".")[1]
     nickname=''.join(random.sample(list_nicknames,1))
     new_name=nickname+"."+after
     list_nicknames.remove(nickname)
-    shutil.copy(path+file_name,path+new_name)
-    f.write(before+" "+"="+" "+nickname+"\n")
-    myZip.writestr(nickname,file.getvalue())
-f.close()
+    myZip.writestr(new_name,file.getvalue())
             
 
 myZip.close()
 
-with open("list_nicknames.zip","rb") as f:
+with open("list_files.zip","rb") as f:
     st.download_button("Télécharger les documents",f,file_name="files.zip")
 
 if st.button("CLEAR"):
