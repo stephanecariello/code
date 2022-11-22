2c1152aa074352677abcab3951675d82f81017cf
stephanecariello <107881867+stephanecariello@users.noreply.github.com>
2022-11-22 22:13:14 +0100

    Add files via upload

--- mypage_rename_randomly.py
+++ mypage_rename_randomly.py
@@ -29,8 +29,7 @@ for file in files:
     name=changeFileName(file.name,nickname)
     myZip.writestr(name,file.getvalue())
     list_nicknames.remove(nickname)
-    before=file.split(".")
-    f.write(before+" "+"="+" "+nickname+"\n")
+    f.write(file.name+" "+"="+" "+nickname+"\n")
 f.close()
  
 myZip.write("nicknames.txt")
